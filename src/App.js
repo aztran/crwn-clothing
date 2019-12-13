abeebd40f007c6d1791cf13b957c9ed0446b2a7b
aztran <andra.riztyan@gmail.com>
2019-12-13 17:45:58 +0700

    adding header & collection item

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
-import HomePage from "./pages/homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
+import Shop from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 const HatsPage = () => (
   <div>
     <h1>Hats Page</h1>
   </div>
 );
+
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={Shop} />
       </Switch>
       {/* <Homepage /> */}
     </div>
